df45cd12cb25326c3e099fca0fd45d1e2f6c618f
Yaakov <yaakovb@nonamesecurity.com>
2021-11-04 08:51:21 +0200

    add playground test site

--- playground/index.js
+++ playground/index.js
@@ -1,2 +1,21 @@
 import Guide from "guidance-core";
-const g = new Guide([{ highlightType: "HARD", type: "TOOLTIP" }]);
+const g = new Guide([
+  {
+    type: "TOOLTIP",
+    element: "#step-1",
+    headerText: "Step 1",
+    bodyText: "Click here for step 1",
+    progressOn: "ELEMENT",
+    highlightType: "HARD",
+  },
+  {
+    type: "TOOLTIP",
+    element: "#step-2",
+    headerText: "Step 2",
+    bodyText: "Click here for step 2",
+    progressOn: "ELEMENT",
+    highlightType: "SOFT",
+  },
+]);
+
+g.start();
