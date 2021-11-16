e1e543e84b87b26f9afd37da910ebe9b36dbd601
Yaakov <yaakovb@nonamesecurity.com>
2021-11-16 21:25:02 +0200

    add some more tests

--- tests/helpers.js
+++ tests/helpers.js
@@ -1,5 +1,12 @@
 module.exports = {
   generateImageName(title) {
-    return title.replace(/\s/g, "-");
+    return title.replace(/\s/g, "-") + ".png";
+  },
+  sleep(ms) {
+    return new Promise((res, rej) => {
+      setTimeout(() => {
+        res();
+      }, ms);
+    });
   },
 };
