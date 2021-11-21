fe02edacd94128d3ce334f19571b4ea64ed6cba6
Yaakov <yaakovb@nonamesecurity.com>
2021-11-21 16:55:46 +0200

    update config

--- core/tests/config.js
+++ core/tests/config.js
@@ -1,6 +1,5 @@
 const SERVER_URL = "http://localhost:5000";
 const PAGES_URL = `${SERVER_URL}/tests/pages`;
-const LIB_URL = `${SERVER_URL}/dist/index.js`;
 const ATTRIBUTE_SELECTOR = `[data-wanderer-id=@@@]`;
 
-module.exports = { SERVER_URL, PAGES_URL, LIB_URL, ATTRIBUTE_SELECTOR };
+module.exports = { SERVER_URL, PAGES_URL, ATTRIBUTE_SELECTOR };
