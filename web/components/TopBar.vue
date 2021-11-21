79b2dd5511fd05744464a064261e27429ec56cf6
Yaakov <yaakovb@nonamesecurity.com>
2021-11-21 12:49:05 +0200

    some config stuff

---
+++ web/components/TopBar.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="h-16 flex items-center px-2 border-b border-gray-200">
+    <div class="h-full flex items-center">
+      <img src="~/static/logo.png" alt="" class="h-3/4" />
+      <div class="text-xl ml-2 font-bold">Wanderer.js</div>
+    </div>
+  </div>
+</template>
--- web/pages/index.vue
+++ web/pages/index.vue
@@ -1,11 +1,23 @@
 <template>
-  <div>
-    <h1 class="text-lg">Interactive Product Tours</h1>
+  <div class="w-full">
+    <TopBar />
+    <div class="flex w-full justify-center">
+      <div class="flex flex-col w-1/2 mt-10">
+        <h1 class="text-4xl font-bold text-center">
+          Reduce user frustration with interactive product tours.
+        </h1>
+        <h3 class="mt-4 text-xl text-center">
+          Let's face it, product tours are an annoying necessity for solo
+          founders. Get it done in an hour with Wanderer.js
+        </h3>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import TopBar from '~/components/TopBar.vue'
 
-export default Vue.extend({})
+export default Vue.extend({ components: { TopBar } })
 </script>
