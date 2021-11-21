f229c7d25cc9276cfe4e4e37d0872bec4ef18130
Yaakov <yaakovb@nonamesecurity.com>
2021-11-21 16:30:37 +0200

    start styling homepage

--- web/pages/index.vue
+++ web/pages/index.vue
@@ -1,16 +1,17 @@
 <template>
   <div class="w-full">
     <TopBar />
-    <div class="flex w-full justify-center">
-      <div class="flex flex-col w-1/2 mt-10">
-        <h1 class="text-4xl font-bold text-center">
+    <div class="flex w-full items-center px-4">
+      <div class="flex flex-col w-1/2 mt-40">
+        <h1 class="text-6xl font-bold">
           Reduce user frustration with interactive product tours.
         </h1>
-        <h3 class="mt-4 text-xl text-center">
+        <h3 class="text-xl mt-5">
           Let's face it, product tours are an annoying necessity for solo
           founders. Get it done in an hour with Wanderer.js
         </h3>
       </div>
+      <div></div>
     </div>
   </div>
 </template>
