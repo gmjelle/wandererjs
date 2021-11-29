518ba12451a235a178886b93cefc6bdd757bdbe7
Yaakov <yaakovb@nonamesecurity.com>
2021-11-29 15:04:27 +0200

    revert

--- web/src/components/Topbar.vue
+++ web/src/components/Topbar.vue
@@ -14,8 +14,8 @@
       z-50
     "
   >
-    <div class="h-full flex items-center">
-      <router-link to="/" class="h-3/4 flex items-center whitespace-nowrap">
+    <div class="flex items-center h-full">
+      <router-link to="/" class="h-3/4 flex items-center">
         <img :src="logo" class="h-full" />
         <div class="text-xl font-bold ml-2">WandererJS</div>
       </router-link>
